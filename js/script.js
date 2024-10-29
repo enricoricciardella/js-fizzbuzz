82b5c618c0e2770a9fa4f2d687c975ef87aa46a7
enricoricciardella <enrico.ricciardella@gmail.com>
2024-10-29 15:12:37 +0100

    Feat:finish

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// alert ("CIAOOO")
+
+//esecuzione logica
+//CREO NUMERI DA 1 A 100
+for(let i = 0; i <= 100; i++){
+    console.log(i); //Debug
+    //CONTROLLO SE SONO DIVISIBILI ENTRAMBI, METTO fizzbuss
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log(`FIZZBUZZ`);
+    }
+    //Controllo se i è multiplo di 3 e scrivo FIZZ
+    else if (i % 3 === 0){
+        console.log(`FRIZZ`);
+        console.log(i); //debug
+    }
+    else if (i % 5 === 0){
+        console.log(`BUZZ`);
+        console.log(i); //debug
+    }
+}
